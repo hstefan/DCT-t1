e1d07bd9ccd348a1a82e747f30e0d348c3bab9db
Hugo Stefan Kaus Puhlmann <hugopuhlmann@gmail.com>
2011-04-07 13:26:15 -0300

    generate a sample arithmetic progression beginning with a random number, with random rate

--- src/sample/LinearGen.hpp
+++ src/sample/LinearGen.hpp
@@ -8,15 +8,21 @@ namespace hstefan
 {
 	namespace sample 
 	{
+		/*
+		 * Gera amostra em progressao aritmetica, com valor inicial aleatorio e razao aleatoria (entre 1 e 20)
+		 */
 		class LinearGen : public SampleGenerator
 		{
+		private:
+			static const int MAX_RATE = 20;
 		public:
 			void generateSample(unsigned int n) 
 			{
-				signal_type start = (signal_type)rand() % (std::numeric_limits<signal_type>::max() - n);
+				const signal_type rate = (signal_type) rand() % MAX_RATE;
+ 				signal_type start = (signal_type)rand() % (std::numeric_limits<signal_type>::max() - n - MAX_RATE);
 				signal.resize(n);
 				signal[0] = start;
-				for(unsigned int i = 1; i < n; i++) 
+				for(unsigned int i = 1; i < n; ++i) 
 					signal[i] = start + i;
 			}
 
