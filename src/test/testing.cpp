de3a552e3b09986b586ce51488500c20e82a6a14
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-06 22:56:54 -0300

    testing system refactored

--- src/test/testing.cpp
+++ src/test/testing.cpp
@@ -1,15 +1,29 @@
 #include "../sample/RandomGen.hpp"
+#include "../sample/LinearGen.hpp"
 #include <iostream>
 
 int main(int argc, char* argv[]) 
 {
-	using hstefan::sample::RandomGen;
 	srand((unsigned int)time(0));
-	RandomGen r;
-	r.generateSample(16);
-	std::vector<RandomGen::signal_type>& vec = r.getSample();
-	for(std::vector<RandomGen::signal_type>::iterator it = vec.begin(); it != vec.end(); ++it) 
-		std::cout << *it << " ";
-	std::cout << std::endl;
+
+	using namespace hstefan::sample;
+	LinearGen* l = new LinearGen();
+	RandomGen* r = new RandomGen();
+
+	std::vector<SampleGenerator*> gen;
+	gen.push_back(l);
+	gen.push_back(r);
+
+	for(std::vector<SampleGenerator*>::iterator it = gen.begin(); it != gen.end(); ++it)
+	{
+		(*it)->generateSample(16);
+		for(std::vector<SampleGenerator::signal_type>::iterator ij = (*it)->getSample().begin(); ij != (*it)->getSample().end(); ++ij) 
+			std::cout << (unsigned int)*ij << " ";
+		std::cout << std::endl;
+	}
+
+	delete l;
+	delete r;
+
 	std::cin.get();
 }
