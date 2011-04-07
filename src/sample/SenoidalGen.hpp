9575fc7ea8ef21e4761cc45ae379f783f33a688a
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-06 22:56:26 -0300

    senoidal sample generation

---
+++ src/sample/SenoidalGen.hpp
@@ -0,0 +1,17 @@
+#ifndef HSTEFAN_SENOIDAL_GEN_HPP
+#define HSTEFAN_SENOIDAL_GEN_HPP
+
+#include "SampleGenerator.hpp"
+
+namespace hstefan
+{
+	namespace sample 
+	{
+		class SenoidalGen : public SampleGenerator
+		{
+
+		};
+	}
+}
+
+#endif
