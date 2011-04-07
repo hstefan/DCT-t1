b03acaec30aecb43baf8bac9bc36d6bf60470259
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-07 19:22:27 -0300

    testing idct

--- src/test/testing.cpp
+++ src/test/testing.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char* argv[]) 
 {
-	srand((unsigned int)time(0));
+	/*srand((unsigned int)time(0));
 
 	using namespace hstefan::sample;
 	LinearGen* l = new LinearGen();
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
 			std::cout << (unsigned int)*ij << " ";
 		std::cout << std::endl;
 	}
-
+	*/
 	std::vector<unsigned char> vs;
 	vs.push_back(8);
 	vs.push_back(16);
@@ -41,11 +41,17 @@ int main(int argc, char* argv[])
 
 	for(std::vector<double>::iterator it = t.begin(); it != t.end(); ++it)
 		std::cout << *it << " ";
+	
+	std::cout << std::endl;
+
+	std::vector<unsigned char> ret = DiscreteCosineTransform::idct(t);
+	for(std::vector<unsigned char>::iterator it = ret.begin(); it != ret.end(); ++it)
+		std::cout << (unsigned int)*it << " ";
 
 	std::cout << std::endl;
 
-	delete l;
-	delete r;
+	/*delete l;
+	delete r;*/
 
 	std::cin.get();
 }
