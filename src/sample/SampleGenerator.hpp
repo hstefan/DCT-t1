fd791083279cbcf6bdad2547d2b4db0cf9ac6792
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-07 07:29:29 -0300

    I decided to documentate each class/method in this project

--- src/sample/SampleGenerator.hpp
+++ src/sample/SampleGenerator.hpp
@@ -7,14 +7,30 @@ namespace hstefan
 {
 	namespace sample
 	{
+		/**
+		 * Classe usada para gerar amostras de sinal. 
+		 */
 		class SampleGenerator 
 		{
 		public:
 			typedef unsigned char signal_type;
+
+			/**
+			 * Gera a amostra propriamente dita.
+			 * @param n Tamanho da amostra.
+			 */
 			virtual void generateSample(unsigned int n) = 0;
+			
+			/**
+			 * Retorna a amostra gerada.
+			 * @return std::vector com a amostra.
+			 */
 			inline std::vector<signal_type>& getSample();
 			inline const std::vector<signal_type>& getSample() const;
 		protected:
+			/**
+			 * Vetor usado para armazenar a amostra.
+			 */
 			std::vector<signal_type> signal;
 		};
 
