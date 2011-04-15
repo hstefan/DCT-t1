4d2dd4ae70d954091a2676523ca6c4e5249e195d
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-15 13:58:31 -0300

    DctController for MVC pattern

--- src/gui/DctController.hpp
+++ src/gui/DctController.hpp
@@ -3,19 +3,39 @@
 
 #include "DctView.hpp"
 #include "DctModel.hpp"
-
+#include "../util/Listener.hpp"
+#include "CoefTable.hpp"
 namespace hstefan
 {
 	namespace gui
 	{
-		class DctController
+		template <class T = CoefTable>
+		class DctController : public util::Listener
 		{
 		public:
-			DctController(DctModel* model, DctView* view);
+			typedef DctModel::output_type output_type;
+			typedef DctModel::signal_type signal_type;
+
+			DctController(T* observing, DctModel* model, DctView* view);
+
+			virtual void notify();
 		private:
 			DctModel* model;
 			DctView* view;
+			T* observing;
 		};
+		template <class T>
+		DctController<T>::DctController(T* observing, DctModel* model, DctView* view)
+			: model(model), view(view),observing(observing)
+		{}
+
+		template <class T>
+		void DctController<T>::notify()
+		{
+			const std::vector<signal_type>& sig = T->getCoefficients();
+			model->setSignal(sig);
+			view->setCoefficients(model->coefficients());
+		}
 	}
 }
 
