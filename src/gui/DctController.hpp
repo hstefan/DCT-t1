c02a3f46c6708a25a9c8e2489ab1291f4b8d2984
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-15 00:08:00 -0300

    DctController first steps

--- src/gui/DctController.hpp
+++ src/gui/DctController.hpp
@@ -1,11 +1,21 @@
 #ifndef HSTEFAN_DCT_CONTROLLER_HPP
 #define HSTEFAN_DCT_CONTROLLER_HPP
 
+#include "DctView.hpp"
+#include "DctModel.hpp"
+
 namespace hstefan
 {
 	namespace gui
 	{
-		
+		class DctController
+		{
+		public:
+			DctController(DctModel* model, DctView* view);
+		private:
+			DctModel* model;
+			DctView* view;
+		};
 	}
 }
 
