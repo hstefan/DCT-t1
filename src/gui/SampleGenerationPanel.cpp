e8cb22476e5a37178abb599c404b2784ab0fe973
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-20 23:48:12 -0300

    implementing a bit of funcionality to SampleGenerationPanel

--- src/gui/SampleGenerationPanel.cpp
+++ src/gui/SampleGenerationPanel.cpp
@@ -5,11 +5,17 @@ using namespace hstefan::gui;
 
 SampleGenerationPanel::SampleGenerationPanel(scv::Point pi, unsigned int width, CoefTable* table)
 	: scv::Panel(pi, width, PANEL_HEIGHT),
-	table_ptr(table), sample_button(), len_label(scv::Point(W_SPACING, H_SPACING), "Tamanho"), 
+	table_ptr(table), sample_button(this), len_label(scv::Point(W_SPACING, H_SPACING), "Tamanho"), 
 	type_label(scv::Point(W_SPACING, H_SPACING*2 + len_label.getHeight()), "Tipo"),
 	sample_box(scv::Point(width - std::max(type_label.getWidth(), len_label.getWidth()), H_SPACING + len_label.getHeight()), COMBO_WIDTH),
-	len_box(scv::Point(width -  std::max(type_label.getWidth(), len_label.getWidth()), H_SPACING*2 + len_label.getHeight()), COMBO_WIDTH)
+	len_box(scv::Point(width -  std::max(type_label.getWidth(), len_label.getWidth()), H_SPACING + len_label.getHeight()*2), COMBO_WIDTH)
 {
+	sample_box.addItem("Aleatoria");
+	sample_box.addItem("Linear");
+	
+	len_box.addItem("8");
+	len_box.addItem("16");
+
 	addComponent(&sample_box);
 	addComponent(&sample_button);
 	addComponent(&type_label);
--- src/gui/SampleGenerationPanel.hpp
+++ src/gui/SampleGenerationPanel.hpp
@@ -16,7 +16,7 @@ namespace hstefan
 		{
 		public:
 			SampleGenerationPanel(scv::Point pi, unsigned int width, CoefTable* table);
-			
+
 			static const unsigned int PANEL_HEIGHT = 50;
 			static const unsigned int W_SPACING = 5;
 			static const unsigned int H_SPACING = 5;
@@ -25,9 +25,25 @@ namespace hstefan
 			class SampleButton : public scv::Button
 			{ 
 			public:
-				virtual void onMouseClick( const scv::MouseEvent &evt );
+				SampleButton(SampleGenerationPanel* pan)
+					: scv::Button(scv::Point(W_SPACING, pan->len_box.getHeight()*3), pan->getWidth() - 2*H_SPACING, "Gerar Amostra"), panel(pan)
+				{
+				}
+				virtual void onMouseClick( const scv::MouseEvent &evt )
+				{
+					panel->generateSample();
+				}
+			private:
+				SampleGenerationPanel* panel;
 			};
+
+			void generateSample()
+			{
+
+			}
 		private:
+			friend class SampleButton;
+
 			scv::Label len_label;
 			scv::Label type_label;
 			scv::ComboBox sample_box;
