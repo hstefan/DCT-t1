5f1ffdb5065779990ad276bfb26706ec426263bc
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-04-21 00:16:30 -0300

    dragon age 2 tiem\!

--- src/gui/SampleGenerationPanel.cpp
+++ src/gui/SampleGenerationPanel.cpp
@@ -7,8 +7,8 @@ SampleGenerationPanel::SampleGenerationPanel(scv::Point pi, unsigned int width, 
 	: scv::Panel(pi, width, PANEL_HEIGHT),
 	table_ptr(table), len_label(scv::Point(W_SPACING, H_SPACING), "Tamanho"),
 	type_label(scv::Point(W_SPACING, H_SPACING*2 + len_label.getHeight()), "Tipo"),
-	sample_box(scv::Point(width - std::max(type_label.getWidth(), len_label.getWidth()), H_SPACING + len_label.getHeight()), COMBO_WIDTH),
-	len_box(scv::Point(width -  std::max(type_label.getWidth(), len_label.getWidth()), H_SPACING + len_label.getHeight()*2), COMBO_WIDTH),
+	sample_box(scv::Point(100, H_SPACING + len_label.getHeight()), COMBO_WIDTH),
+	len_box(scv::Point(100, len_label.getWidth()) + H_SPACING + len_label.getHeight()*2, COMBO_WIDTH),
 	sample_button(this, len_label.getHeight()*3 + H_SPACING, getWidth() - 2*W_SPACING)
 {
 	sample_box.addItem("Aleatoria");
@@ -20,6 +20,5 @@ SampleGenerationPanel::SampleGenerationPanel(scv::Point pi, unsigned int width, 
 	addComponent(&sample_box);
 	addComponent(&sample_button);
 	addComponent(&type_label);
-	//addComponent(&sample_button);
 	addComponent(&len_box);
 }
